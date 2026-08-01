4a29e0dcee86a528524128293f04a1fd3315b51d
Kenji Saito <ken-yo@mbr.nifty.com>
2026-08-01 17:42:43 +0900

    example の cfn stack を main と分離

--- example/bin/example.ts
+++ example/bin/example.ts
@@ -3,7 +3,7 @@ import * as cdk from 'aws-cdk-lib';
 import { ExampleStack } from '../lib/example-stack.js';
 
 const app = new cdk.App();
-const stack = new ExampleStack(app, 'AwsLambdaMcpServerExample', {
+const stack = new ExampleStack(app, 'AwsLambdaMcpServerExampleV1', {
 
 });
 cdk.RemovalPolicies.of(stack).destroy();
